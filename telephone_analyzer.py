e51b678ad6daff0a5587ac85435195f52afeae4b
Manuel Pol <manus@Portatil.(none)>
2012-12-13 19:11:07 +0100

    Pre remove refresh button.

--- telephone_analyzer.py
+++ telephone_analyzer.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 import csv
-import sys
 import pygtk
 import gtk
 import gobject
@@ -14,42 +13,81 @@ class main:
         builder = gtk.Builder()
         builder.add_from_file("mainView.ui")
 
-        links = {}
+        links = {"on_window1_destroy": self.QuitAll,
+        "on_button_start_clicked": self.LaunchCreate,
+        "on_filechooserbutton1_file_set": self.OpenFile,
+        "on_entry1_insert_text": self.LaunchMessage,
+        "on_button_refresh_clicked": self.Refresh}
 
         builder.connect_signals(links)
 
         self.filefilter = builder.get_object("filefilter")
+        self.filechooser = builder.get_object("filechooserbutton1")
+        self.statusbar = builder.get_object("statusbar1")
+        self.entrytext = builder.get_object("entry1")
+        self.filefolder = builder.get_object("filechooserbutton2")
+
+        self.context_id = self.statusbar.get_context_id("A")
+        self.statusbar.push(self.context_id, "Selecciona fichero origen .cdr")
 
         self.filefilter.add_pattern("*.cdr")
         self.Tels = {}
+        self.selectedfile = 0
 
-    def OpenFile(self):
+    def LaunchCreate(self, widget):
+        if self.selectedfile:
+            if self.entrytext.get_text_length():
+                self.ShowMessage("Generando ficheros")
+                self.GenerateFiles()
+            else:
+                self.ShowMessage("Nombre de fichero final no seleccionado")
+        else:
+            self.ShowMessage("Debe seleccionar un fichero .cdr origen")
+
+    def OpenFile(self, widget):
+        fileName = self.filechooser.get_filename()
         try:
-            fileR = open(sys.argv[1], "r")
+            fileR = open(fileName, "r")
         except IOError:
-            print "\nNombre del fichero no válido \n"
+            self.ShowMessage("Imposible abrir fichero")
         except:
-            print "\nDebe introducir el nombre del fichero \n"
-
-        self.fileC = csv.reader(fileR, delimiter=',')
+            self.ShowMessage("Imposible abrir fichero")
+        else:
+            self.fileC = csv.reader(fileR, delimiter=',')
+            self.selectedfile = 1
+            self.AnalyzeFile()
 
     def AnalyzeFile(self):
-        print "ORIGEN , DESTINO , SEGUNDOS, PRECIO CARRIER, PRECIO CLIENTE,NÚMERO MARCADO, TIEMPO GRATIS CARRIER"
+        #print "ORIGEN , DESTINO , SEGUNDOS, PRECIO CARRIER, PRECIO CLIENTE,NÚMERO MARCADO, TIEMPO GRATIS CARRIER"
         for row in self.fileC:
             if row[9] in self.Tels:
                 self.Tels[row[9]].append([row[19].strip("\'"), float(row[28].strip("\'")), float(row[33].strip("\'"))])
             else:
                 self.Tels[row[9]] = [[row[19].strip("\'"), row[28].strip("\'"), row[33].strip("\'")]]
 
-        #print Tels
+        self.ShowMessage("Introduce nombre ficheros destino")
 
-        for row in self.Tels:
-            print row
-            print self.Tels[row][0][0]
-            print self.Tels[row][1][0]
+    def GenerateFiles(self):
+        print self.filefolder.get_filename()
+        print self.entrytext.get_text()
+        print self.filechooser.get_filename()
 
-    def QuitAll(self):
-        print "Cerrada"
+    def Refresh(self, widget):
+        self.Tels = {}
+        self.selectedfile = 0
+        self.entrytext.set_text("")
+        self.filechooser.set_current_folder("")
+        self.ShowMessage("Selecciona fichero origen .cdr")
+
+    def LaunchMessage(self, *widget):
+        self.ShowMessage("Selecciona \"Generar Ficheros\" para finalizar")
+
+    def ShowMessage(self, message):
+        self.statusbar.pop(self.context_id)
+        self.statusbar.push(self.context_id, message)
+
+    def QuitAll(self, widget):
+        gtk.main_quit()
 
 
 if __name__ == '__main__':
